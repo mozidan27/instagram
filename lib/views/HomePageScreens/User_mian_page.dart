155a8de6872a5205491f4e7cb05a60bb44b77034
mozidan27 <153581941+mozidan27@users.noreply.github.com>
2024-04-03 02:44:26 +0200

    update on hte user main page , all i did changing the appbar to sliver appbar

--- lib/views/HomePageScreens/User_mian_page.dart
+++ lib/views/HomePageScreens/User_mian_page.dart
@@ -3,7 +3,9 @@ import 'package:instagram/views/utils/bubbles_stories.dart';
 import 'package:instagram/views/utils/user_posts.dart';
 
 class UserMainPage extends StatelessWidget {
-  final List people = [
+  UserMainPage({super.key, Key});
+
+  final List<String> people = [
     'Mohamed',
     'Sayed',
     'Mohamed',
@@ -13,59 +15,65 @@ class UserMainPage extends StatelessWidget {
     'Ali'
   ];
 
-  UserMainPage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        elevation: 0,
-        title: SizedBox(
-          height: 80,
-          child: Image.asset(
-            'assets/images/textlogo.png',
-            color: Colors.white,
-          ),
-        ),
-        actions: const [
-          Icon(
-            Icons.favorite_border,
-            size: 32,
-            color: Colors.white,
-          ),
-          SizedBox(
-            width: 30,
-          ),
-          Icon(
-            Icons.maps_ugc_outlined,
-            size: 32,
-            color: Colors.white,
-          ),
-          SizedBox(
-            width: 10,
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 150,
-            child: ListView.builder(
-              itemCount: people.length,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) {
-                return BubblesStories(text: people[index]);
-              },
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            floating: true,
+            automaticallyImplyLeading: false,
+            title: SizedBox(
+              height: 80,
+              child: Image.asset(
+                'assets/images/textlogo.png',
+                color: Colors.white,
+              ),
             ),
+            actions: const [
+              Icon(
+                Icons.favorite_border,
+                size: 32,
+                color: Colors.white,
+              ),
+              SizedBox(
+                width: 30,
+              ),
+              Icon(
+                Icons.maps_ugc_outlined,
+                size: 32,
+                color: Colors.white,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+            ],
           ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: people.length,
-              itemBuilder: (context, index) {
-                return UserPost(
-                  username: people[index],
-                );
-              },
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 150,
+                  child: ListView.builder(
+                    itemCount: people.length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return BubblesStories(text: people[index]);
+                    },
+                  ),
+                ),
+                ListView.builder(
+                  padding: EdgeInsets.zero,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: people.length,
+                  itemBuilder: (context, index) {
+                    return UserPost(
+                      username: people[index],
+                    );
+                  },
+                ),
+              ],
             ),
           ),
         ],
